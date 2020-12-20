bf508f53a89a01f4784a617b60c22faed6674440
ph1001 <54173557+ph1001@users.noreply.github.com>
2020-12-20 21:58:51 +0000

    H

--- 02 Code/04 - 20 Dec 2020/task_H.sql
+++ 02 Code/04 - 20 Dec 2020/task_H.sql
@@ -7,41 +7,52 @@ Create two views to recreate the information on the INVOICE (one for the head an
 
 /* Create the view for the invoice header and totals */
 
+/* Create a table to store a single variable */
+drop table if exists `parameter_table`;
+CREATE TABLE IF NOT EXISTS `parameter_table` (
+	`parameter` INTEGER unsigned NOT NULL,
+    primary key (`parameter`)
+);
+insert into `parameter_table`(`parameter`) values (1);
+
 CREATE OR REPLACE VIEW shoeshop.invoice_head_totals
 AS
 select
 customer_order_and_invoice_ID as 'INVOICE NUMBER',
 invoice_date as 'DATE OF ISSUE',
 invoice_address as 'BILLED TO',
-'Shoes24, Rua do Forno do Tijolo 73-65, 1170-172 Lisbon, Portugal, +351 639 230 003, shoes24@email.com, shoes24.com' as 'our info',
-# Subtotal = (unit_costs * quantities)
-(
-select sum(s.unit_price * coi.quantity) 
-from  stock s, customer_order_item coi 
-where s.product_ID = coi.product_ID
-) as 'SUBTOTAL',
+'Sneakersly, Rua do Forno do Tijolo 73-65, 1170-172 Lisbon, Portugal, +351 639 230 003, shoes24@email.com, shoes24.com' as 'our info',
+# Subtotal = sum(values at the time of purchase)
+(select sum(coi.value_at_time) 
+from  customer_order_item coi
+where coi.customer_order_and_invoice_ID=(select * from parameter_table)) 
+as 'SUBTOTAL',
 # Discount [EUR] = discount [%] / 100 * subtotal
-(
-select sum(coi.discount_percent/100 * s.unit_price * coi.quantity)
-from  stock s, customer_order_item coi 
-where s.product_ID = coi.product_ID
-) as 'DISCOUNT',
+(select sum(coi.discount_percent/100 * coi.value_at_time)
+from  customer_order_item coi
+where coi.customer_order_and_invoice_ID=(select * from parameter_table))
+as 'DISCOUNT',
 # Tax rate
-(
-select distinct (co.tax_rate_percent/100) 
+(select distinct (co.tax_rate_percent/100) 
+from customer_order co 
+where co.customer_order_and_invoice_ID = (select * from parameter_table)) 
+as '(TAX RATE)',
+# Tax = tax rate * (subtotal - Discounts)
+((select distinct (co.tax_rate_percent/100) 
 from customer_order_item coi , customer_order co 
-where coi.customer_order_and_invoice_ID = co.customer_order_and_invoice_ID
-) as '(TAX RATE)',
-# Tax
-(
-(select distinct (co.tax_rate_percent/100) from customer_order_item coi , customer_order co where coi.customer_order_and_invoice_ID = co.customer_order_and_invoice_ID)
-* (select (sum(s.unit_price * coi.quantity - coi.discount_percent/100 * s.unit_price * coi.quantity)) from  stock s, customer_order_item coi where s.product_ID = coi.product_ID)
-) as 'TAX',
-# Total = (1 + tax_rate) * ((unit_costs * quantities) - discounts)
-(
-(select distinct (1 + co.tax_rate_percent/100) from customer_order_item coi , customer_order co where coi.customer_order_and_invoice_ID = co.customer_order_and_invoice_ID)
-* (select (sum(s.unit_price * coi.quantity - coi.discount_percent/100 * s.unit_price * coi.quantity)) from  stock s, customer_order_item coi where s.product_ID = coi.product_ID)
-) as 'INVOICE TOTAL'
+where coi.customer_order_and_invoice_ID = co.customer_order_and_invoice_ID and co.customer_order_and_invoice_ID = (select * from parameter_table))
+* (select (sum(coi.value_at_time) - sum(coi.value_at_time * coi.discount_percent/100)) 
+from  customer_order_item coi 
+where coi.customer_order_and_invoice_ID=(select * from parameter_table)))
+as 'TAX',
+# Total = (1 + tax_rate) * (subtotal - discounts)
+((select distinct (1 + co.tax_rate_percent/100) 
+from customer_order_item coi , customer_order co 
+where coi.customer_order_and_invoice_ID = co.customer_order_and_invoice_ID and co.customer_order_and_invoice_ID = (select * from parameter_table))
+* (select (sum(coi.value_at_time) - sum(coi.value_at_time * coi.discount_percent/100)) 
+from  customer_order_item coi 
+where coi.customer_order_and_invoice_ID=(select * from parameter_table)))
+as 'INVOICE TOTAL'
 from customer_order
 where customer_order_and_invoice_ID = 1;
 
