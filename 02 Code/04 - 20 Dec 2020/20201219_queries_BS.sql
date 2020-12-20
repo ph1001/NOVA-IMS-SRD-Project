6a63efd522b6dbf00d5bad3777a2abea46dffe91
Berfin Sakallioglu <berfin.sakallioglu@gmail.com>
2020-12-20 21:06:29 +0000

    value_at_time is being used from now

--- 02 Code/04 - 20 Dec 2020/20201219_queries_BS.sql
+++ 02 Code/04 - 20 Dec 2020/20201219_queries_BS.sql
@@ -11,15 +11,13 @@ where customer_order.order_date between '2020-10_01' and '2020-12_30'
 group by customer.name;
 
 
-
 #G.2
-select customer.name as Best_Customers, concat(sum(stock.unit_price), ' €') as Purchase_Amount
+select customer.name as Best_Customers, concat(sum(customer_order_item.value_at_time), ' €') as Purchase_Amount
 from customer
 join customer_order on customer.customer_ID = customer_order.customer_ID 
 join customer_order_item on customer_order_item.customer_order_and_invoice_ID = customer_order.customer_order_and_invoice_ID
-join stock on stock.product_ID = customer_order_item.product_ID
 group by customer.name
-order by sum(stock.unit_price) desc
+order by sum(customer_order_item.value_at_time) desc
 limit 3;
 
 # G.3
@@ -32,10 +30,9 @@ join customer_order_item on customer_order_item.customer_order_and_invoice_ID = 
 join stock on stock.product_ID = customer_order_item.product_ID;
 
 # G.4
-select substring_index(shipping_address, ',', -2) as City, sum(stock.unit_price) as Total_Sales
+select substring_index(shipping_address, ',', -2) as City, concat(sum(customer_order_item.value_at_time), ' €') as Total_Sales
 from customer_order 
 join customer_order_item on customer_order_item.customer_order_and_invoice_ID = customer_order.customer_order_and_invoice_ID
-join stock on stock.product_ID = customer_order_item.product_ID
 group by City;
 
 
@@ -43,5 +40,4 @@ group by City;
 select substring_index(shipping_address, ',', -2) as City, round(avg(rating),2) as Average_Rating
 from customer_order
 join customer_order_item on customer_order_item.customer_order_and_invoice_ID = customer_order.customer_order_and_invoice_ID
-join stock on stock.product_ID = customer_order_item.product_ID
 group by City;
