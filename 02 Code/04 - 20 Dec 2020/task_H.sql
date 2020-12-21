bea1606fb26fc413515cdf72f51b574f4bc50321
ph1001 <54173557+ph1001@users.noreply.github.com>
2020-12-21 08:39:15 +0000

    Bugfix task H

--- 02 Code/04 - 20 Dec 2020/task_H.sql
+++ 02 Code/04 - 20 Dec 2020/task_H.sql
@@ -7,13 +7,21 @@ Create two views to recreate the information on the INVOICE (one for the head an
 
 /* Create the view for the invoice header and totals */
 
-/* Create a table to store a single variable */
+/* In order to parameterise the view, create a variable representing the ID of the order for which an invoice shall be created.
+For this, first set the variable to the desired value: */
+set @order_ID = 4;
+
+/* Then, create a table to store the value of this variable. For some reason, MySQL does not allow for the variable to be passed directly
+to the views. */
 drop table if exists `parameter_table`;
 CREATE TABLE IF NOT EXISTS `parameter_table` (
 	`parameter` INTEGER unsigned NOT NULL,
     primary key (`parameter`)
 );
-insert into `parameter_table`(`parameter`) values (1);
+insert into `parameter_table`(`parameter`) values (@order_ID);
+
+/* Check the parameter */
+# select * from parameter_table;
 
 CREATE OR REPLACE VIEW shoeshop.invoice_head_totals
 AS
@@ -21,7 +29,7 @@ select
 customer_order_and_invoice_ID as 'INVOICE NUMBER',
 invoice_date as 'DATE OF ISSUE',
 invoice_address as 'BILLED TO',
-'Sneakersly, Rua do Forno do Tijolo 73-65, 1170-172 Lisbon, Portugal, +351 639 230 003, shoes24@email.com, shoes24.com' as 'our info',
+'Sneakersly, Rua do Forno do Tijolo 73-65, 1170-172 Lisbon, Portugal, +351 639 230 003, sneakersly@office.com, sneakersly.com' as 'our info',
 # Subtotal = sum(values at the time of purchase)
 (select sum(coi.value_at_time) 
 from  customer_order_item coi
@@ -54,7 +62,7 @@ from  customer_order_item coi
 where coi.customer_order_and_invoice_ID=(select * from parameter_table)))
 as 'INVOICE TOTAL'
 from customer_order
-where customer_order_and_invoice_ID = 1;
+where customer_order_and_invoice_ID = (select * from parameter_table);
 
 /* Select this view */
 select * from invoice_head_totals;
